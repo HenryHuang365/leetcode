0ee41ee0032f1b162eca2c7d3f83f268b195e634
Jing Huang <jinghuang365@gmail.com>
2024-01-28 22:07:53 +1030

    leetcode/Easy: Q21 merge two sorted list

--- Easy/21-Merge-Two-Sorted-Lists.py
+++ Easy/21-Merge-Two-Sorted-Lists.py
@@ -22,3 +22,25 @@
 # -100 <= Node.val <= 100
 # Both list1 and list2 are sorted in non-decreasing order.
 
+def mergeTwoLists(self, list1, list2):
+    """
+    :type list1: Optional[ListNode]
+    :type list2: Optional[ListNode]
+    :rtype: Optional[ListNode]
+    """
+    
+    curr = dummy = ListNode()
+    while (list1 and list2):
+        if list1.val < list2.val:
+            curr.next = list1
+            curr = list1
+            list1 = list1.next
+        else: 
+            curr.next = list2
+            curr = list2
+            list2 = list2.next
+        
+    if list1 or list2: 
+        curr.next = list1 if list1 else list2
+        
+    return dummy.next
