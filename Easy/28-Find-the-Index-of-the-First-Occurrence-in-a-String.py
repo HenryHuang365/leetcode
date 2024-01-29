ecee186a60cf8f20147bdca9cbf1228a5e8caac9
Jing Huang <jinghuang365@gmail.com>
2024-01-29 17:20:50 +1030

    leetcode/Easy: Q28 solved

--- Easy/28-Find-the-Index-of-the-First-Occurrence-in-a-String.py
+++ Easy/28-Find-the-Index-of-the-First-Occurrence-in-a-String.py
@@ -26,4 +26,9 @@ def strStr(self, haystack, needle):
     :type needle: str
     :rtype: int
     """
-    return 0
+    len_needle = len(needle)
+    for i in range(len(haystack) - len_needle + 1):
+        if haystack[i:i+len_needle] == needle:
+            return i
+
+    return -1
