b05621bd814774c98b681f231647db10a976e105
Jing Huang <jinghuang365@gmail.com>
2024-02-04 18:53:56 +1030

    leetcode/Easy: Q88 solved

--- Easy/100/88-Merge-Sorted-Array.py
+++ Easy/100/88-Merge-Sorted-Array.py
@@ -46,4 +46,9 @@ def merge(self, nums1, m, nums2, n):
     :type n: int
     :rtype: None Do not return anything, modify nums1 in-place instead.
     """
-    return 0
+    j = 0
+    for i in range(m, m+n):
+        nums1[i] = nums2[j]
+        j += 1
+        
+    nums1.sort()
