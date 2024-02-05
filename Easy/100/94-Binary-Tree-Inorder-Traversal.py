7af10bcf50789bef4cf9da057fbc562df2647de7
Jing Huang <jinghuang365@gmail.com>
2024-02-05 21:01:03 +1030

    leetcode/Easy: Q94 solved

--- Easy/100/94-Binary-Tree-Inorder-Traversal.py
+++ Easy/100/94-Binary-Tree-Inorder-Traversal.py
@@ -27,4 +27,14 @@ def inorderTraversal(self, root):
     :type root: TreeNode
     :rtype: List[int]
     """
-    return root
+    result = []
+    def helper(root):
+        if root.left:
+            helper(root.left)
+        result.append(root.val)
+        if root.right:
+            helper(root.right)
+            
+    if root:
+        helper(root)
+    return result
