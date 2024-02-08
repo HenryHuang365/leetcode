7605e44a17db3b985f5d275924a7aaed45201b8d
Jing Huang <jinghuang365@gmail.com>
2024-02-08 23:52:33 +1030

    leetcode/Easy: Q104 solved

--- Easy/200/104-Maximum-Depth-of-Binary-Tree.java
+++ Easy/200/104-Maximum-Depth-of-Binary-Tree.java
@@ -38,6 +38,13 @@ class Solution {
     }
 
     public int maxDepth(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+
+        int maxLeft = maxDepth(root.left);
+        int maxRight = maxDepth(root.right);
+
+        return Math.max(maxLeft, maxRight) + 1;
     }
 }
