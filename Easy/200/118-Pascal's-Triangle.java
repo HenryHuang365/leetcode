8d57dbed4077e37d4439cbebba191bbbe2f02fde
Jing Huang <jinghuang365@gmail.com>
2024-02-13 22:16:51 +1030

    leetcode/Easy: Q118 solved

--- Easy/200/118-Pascal's-Triangle.java
+++ Easy/200/118-Pascal's-Triangle.java
@@ -24,6 +24,28 @@ import java.util.List;
 
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        return new ArrayList<>();
+        if (numRows == 0) {return new ArrayList<>();}
+        if (numRows == 1) {
+            List<List<Integer>> rows = new ArrayList<>();
+            List<Integer> row = new ArrayList<Integer>();
+            row.add(1);
+            rows.add(row);
+            return rows;
+        }
+        
+        List<List<Integer>> prevRows = generate(numRows - 1);
+        List<Integer> newRow = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            newRow.add(1);
+        }
+
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i-1) + prevRows.get(numRows - 2).get(i));
+        }
+
+        prevRows.add(newRow);
+        return prevRows;
     }
+
 }
