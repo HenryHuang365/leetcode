f7c83e95762f6637ea40dc48631eadc1307a77dd
Jing Huang <jinghuang365@gmail.com>
2024-02-18 17:38:13 +1100

    leetcode/Medium: Q 122 solved with violent algo, there exists better algo

--- Easy/200/122-Best-Time-to-Buy-and-Sell-Stock-II.java
+++ Easy/200/122-Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -34,6 +34,10 @@
 
 class Solution {
     public int maxProfit(int[] prices) {
-        return 0;
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            profit += Math.max(prices[i] - prices[i-1], 0);
+        }
+        return profit;
     }
 }
