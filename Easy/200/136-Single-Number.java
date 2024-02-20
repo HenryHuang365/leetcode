f6bd42f54a18ea41a39fe4361d43b829644a847b
Jing Huang <jinghuang365@gmail.com>
2024-02-20 23:40:13 +1100

    leetcode/East: Q136 solved with question about bit operation

--- Easy/200/136-Single-Number.java
+++ Easy/200/136-Single-Number.java
@@ -27,6 +27,10 @@
 
 class Solution {
     public int singleNumber(int[] nums) {
-        return 0;
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
     }
 }
