4381fb7745397f8583490f5220d0c4e633415891
Jing Huang <jinghuang365@gmail.com>
2024-03-14 17:53:43 +1100

    leetcode/Array: Q217 should sort the given int[] before looping

--- Array/217-Contains-Duplicate.java
+++ Array/217-Contains-Duplicate.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         int len1 = nums.length;
+        Arrays.sort(nums);
         for (int i = 0; i < len1 - 1; i++) {
             if (nums[i] == nums[i+1]) {
                 return true;
             }
         }
-            
+
         return false;
     }
 }
