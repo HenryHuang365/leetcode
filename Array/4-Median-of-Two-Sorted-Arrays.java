a0361f4cb9d206f337ddf915311d00af5024161b
HenryHuang365 <jinghuang365@gmail.com>
2024-04-16 10:48:42 +1000

    leetcode/Arrays: Q4 median of two sorted array is finished

--- Array/4-Median-of-Two-Sorted-Arrays.java
+++ Array/4-Median-of-Two-Sorted-Arrays.java
@@ -23,41 +23,78 @@
 // -106 <= nums1[i], nums2[i] <= 106
 
 class Solution {
+    // public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    // int m = nums1.length;
+    // int n = nums2.length;
+    // int len = m + n;
+    // if (len % 2 == 0) {
+    // double left = (double) findKthHelper(nums1, 0, nums2, 0, len / 2);
+    // double right = (double) findKthHelper(nums1, 0, nums2, 0, len / 2 + 1);
+    // return (double) (left + right) / 2;
+    // } else {
+    // return findKthHelper(nums1, 0, nums2, 0, len / 2 + 1);
+    // }
+    // }
+
+    // public int findKthHelper(int[] A, int aStart, int[] B, int bStart, int k) {
+    // if (aStart >= A.length) {
+    // return B[bStart + k - 1];
+    // }
+
+    // if (bStart >= B.length) {
+    // return A[aStart + k - 1];
+    // }
+
+    // if (k == 1) {
+    // return Math.min(A[aStart], B[bStart]);
+    // }
+
+    // int aMid = aStart + k / 2 - 1;
+    // int bMid = bStart + k / 2 - 1;
+    // int aVal = aMid >= A.length ? Integer.MAX_VALUE : A[aMid];
+    // int bVal = bMid >= B.length ? Integer.MAX_VALUE : B[bMid];
+
+    // if (aVal <= bVal) {
+    // return findKthHelper(A, aMid + 1, B, bStart, k - k / 2);
+    // } else {
+    // return findKthHelper(A, aStart, B, bMid + 1, k - k / 2);
+    // }
+    // }
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int len = m + n;
-        if (len % 2 == 0) {
-            double left = (double) findKthHelper(nums1, 0, nums2, 0, len / 2);
-            double right = (double) findKthHelper(nums1, 0, nums2, 0, len / 2 + 1);
-            return (double) (left + right) / 2;
-        } else {
-            return findKthHelper(nums1, 0, nums2, 0, len / 2 + 1);
-        }
-    }
-
-    public int findKthHelper(int[] A, int aStart, int[] B, int bStart, int k) {
-        if (aStart >= A.length) {
-            return B[bStart + k - 1];
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        if (bStart >= B.length) {
-            return A[aStart + k - 1];
+        int k = (n1 + n2 + 1) / 2;
+
+        int l = 0;
+        int r = n1;
+
+        while (l < r) {
+            int m1 = l + (r - l) / 2;
+            int m2 = k - m1;
+            if (nums1[m1] < nums2[m2 - 1]) {
+                l = m1 + 1;
+            } else {
+                r = m1;
+            }
         }
 
-        if (k == 1) {
-            return Math.min(A[aStart], B[bStart]);
+        int m1 = l;
+        int m2 = k - l;
+
+        int c1 = Math.max(m1 <= 0 ? Integer.MIN_VALUE : nums1[m1 - 1],
+                m2 <= 0 ? Integer.MIN_VALUE : nums2[m2 - 1]);
+
+        if ((n1 + n2) % 2 == 1) {
+            return c1;
         }
 
-        int aMid = aStart + k / 2 - 1;
-        int bMid = bStart + k / 2 - 1;
-        int aVal = aMid >= A.length ? Integer.MAX_VALUE : A[aMid];
-        int bVal = bMid >= B.length ? Integer.MAX_VALUE : B[bMid];
-
-        if (aVal <= bVal) {
-            return findKthHelper(A, aMid + 1, B, bStart, k - k / 2);
-        } else {
-            return findKthHelper(A, aStart, B, bMid + 1, k - k / 2);
-        }
+        int c2 = Math.min(m1 >= n1 ? Integer.MAX_VALUE : nums1[m1],
+                m2 >= n2 ? Integer.MAX_VALUE : nums2[m2]);
+        return (c1 + c2) * 0.5;
     }
 }
