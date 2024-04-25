73ace5329ee1ac6c8da1025b04004af1866ed5ae
HenryHuang365 <jinghuang365@gmail.com>
2024-04-25 15:38:40 +1000

    leetcode/Array: Q90 subsets II skip dupicate elements

---
+++ Array/90-Subsets-II.java
@@ -0,0 +1,43 @@
+// Given an integer array nums that may contain duplicates, return all possible 
+// subsets
+//  (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+// Example 1:
+
+// Input: nums = [1,2,2]
+// Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [[],[0]]
+
+// Constraints:
+
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    public void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1])
+                continue;
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
