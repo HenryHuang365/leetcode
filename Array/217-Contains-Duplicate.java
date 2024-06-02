9f5a77d112a239c3464939d3db6b7a33781a6357
HenryHuang365 <jinghuang365@gmail.com>
2024-06-03 02:28:59 +1000

    leetcode/Array: Q217 done in java with Set

--- Array/217-Contains-Duplicate.java
+++ Array/217-Contains-Duplicate.java
@@ -1,14 +1,30 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         int len1 = nums.length;
         Arrays.sort(nums);
         for (int i = 0; i < len1 - 1; i++) {
-            if (nums[i] == nums[i+1]) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
         }
 
+        return false;
+    }
+
+    public boolean containsDuplicate1(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+
+        for (int num : nums) {
+            if (numSet.contains(num)) {
+                return true;
+            }
+
+            numSet.add(num);
+        }
         return false;
     }
 }
