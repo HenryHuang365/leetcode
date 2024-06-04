8504e120246911254a0576fac3a140135a518133
HenryHuang365 <jinghuang365@gmail.com>
2024-06-04 22:01:46 +1000

    leetcode/string: Q242 done in java with hashmap

--- String/242-Valid-anagram.java
+++ String/242-Valid-anagram.java
@@ -22,9 +22,27 @@
 
 // 1 <= s.length, t.length <= 5 * 104
 // s and t consist of lowercase English letters.
+import java.util.HashMap;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
-        return false;
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        for (char c : countS.keySet()) {
+            if (!countS.get(c).equals(countT.get(c)) || !countT.containsKey(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
