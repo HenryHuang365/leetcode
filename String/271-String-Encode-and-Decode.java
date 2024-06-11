9edd0376e6c585f743e2d78b0d3dfd251544b305
HenryHuang365 <jinghuang365@gmail.com>
2024-06-11 23:35:09 +1000

    leetcode/string: Q271 spec with java

---
+++ String/271-String-Encode-and-Decode.java
@@ -0,0 +1,34 @@
+// Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+// Please implement encode and decode
+
+// Example 1:
+
+// Input: ["neet","code","love","you"]
+
+// Output:["neet","code","love","you"]
+// Example 2:
+
+// Input: ["we","say",":","yes"]
+
+// Output: ["we","say",":","yes"]
+// Constraints:
+
+// 0 <= strs.length < 100
+// 0 <= strs[i].length < 200
+// strs[i] contains only UTF-8 characters.
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+
+    public String encode(List<String> strs) {
+        return "";
+    }
+
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+        return list;
+    }
+}
