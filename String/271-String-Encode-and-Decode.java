6d3286eb61565fd1df351328b34b5ef5a5c0c992
HenryHuang365 <jinghuang365@gmail.com>
2024-06-13 21:42:58 +1000

    leetcode/string: Q271 done in java

--- String/271-String-Encode-and-Decode.java
+++ String/271-String-Encode-and-Decode.java
@@ -24,11 +24,29 @@ import java.util.ArrayList;
 class Solution {
 
     public String encode(List<String> strs) {
-        return "";
+        StringBuilder encodedString = new StringBuilder();
+
+        for (String str : strs) {
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return encodedString.toString();
     }
 
     public List<String> decode(String str) {
+        System.out.println("str: " + str);
         List<String> list = new ArrayList<>();
+        int i = 0;
+
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+
+            int len = Integer.parseInt(str.substring(i, j));
+            i = j + 1 + len;
+            list.add(str.substring(j + 1, i));
+        }
         return list;
     }
 }
