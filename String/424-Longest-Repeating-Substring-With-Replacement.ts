aadedc38ad32bae662e1ddceb2e83e4278e205c4
Jing Huang <jinghuang365@gmail.com>
2024-06-26 21:23:51 +1000

    leetcode/string: Q424 done in ts

--- String/424-Longest-Repeating-Substring-With-Replacement.ts
+++ String/424-Longest-Repeating-Substring-With-Replacement.ts
@@ -1,18 +1,18 @@
 function characterReplacement(s: string, k: number): number {
     let list = new Map();
     let res = 0;
+    let max = 0;
     let l = 0;
 
-    for (let r = 0; r < s.length; r++) {     
-        let len = r - l + 1;   
-        list.set(s[r], 1 + (list.get(s[r]) || 0));  
+    for (let r = 0; r < s.length; r++) {             
+        list.set(s[r], 1 + (list.get(s[r]) || 0)); 
+        max = Math.max(max, list.get(s[r]));
 
-        if (len - Math.max(...list.values()) > k) {
+        if ((r - l + 1) - max > k) {
             list.set(s[l], list.get(s[l]) - 1);
             l++;
-        }
-        len = r - l + 1;
-        res = Math.max(res, len);
+        }        
+        res = Math.max(res, r - l + 1);
     }
     return res;
 };
