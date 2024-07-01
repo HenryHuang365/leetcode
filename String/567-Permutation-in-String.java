d623cbed65ac81ec028d5b8e62cd71eb4df81db1
HenryHuang365 <jinghuang365@gmail.com>
2024-07-01 21:21:18 +1000

    leetcode/string: Q567 done in java

--- String/567-Permutation-in-String.java
+++ String/567-Permutation-in-String.java
@@ -2,8 +2,6 @@
 
 // In other words, return true if one of s1's permutations is the substring of s2.
 
- 
-
 // Example 1:
 
 // Input: s1 = "ab", s2 = "eidbaooo"
@@ -13,7 +11,6 @@
 
 // Input: s1 = "ab", s2 = "eidboaoo"
 // Output: false
- 
 
 // Constraints:
 
@@ -22,6 +19,47 @@
 
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        return false;
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+
+        int matches = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] == s2Count[i]) {
+                matches++;
+            }
+        }
+
+        int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+            if (matches == 26) {
+                return true;
+            }
+
+            int index = s2.charAt(r) - 'a';
+            s2Count[index]++;
+            if (s1Count[index] == s2Count[index]) {
+                matches++;
+            } else if (s1Count[index] + 1 == s2Count[index]) {
+                matches--;
+            }
+
+            index = s2.charAt(l) - 'a';
+            s2Count[index]--;
+            if (s1Count[index] == s2Count[index]) {
+                matches++;
+            } else if (s1Count[index] - 1 == s2Count[index]) {
+                matches--;
+            }
+            l++;
+        }
+        return matches == 26;
     }
 }
