cb904976fdf11666ff281b19ac39b7cbe331b3df
HenryHuang365 <jinghuang365@gmail.com>
2024-07-08 12:28:34 +1000

    leetcode/array: Q219 and 594 redo in java

--- Array/219-Contains-Duplicate-II.java
+++ Array/219-Contains-Duplicate-II.java
@@ -3,22 +3,22 @@ import java.util.HashSet;
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set<Integer> subSet = new HashSet<>();
+        Set<Integer> subSets = new HashSet<>();
         int l = 0;
-
         for (int r = 0; r < nums.length; r++) {
             if (Math.abs(l - r) > k) {
-                subSet.remove(nums[l]);
+                subSets.remove(nums[l]);
                 l++;
             }
+
             // Mistake: I used nums[l] == nums[r] to determine if we can return true.
             // Note that i, j can be any number in the window, while l and r are the start
             // and end of the window.
-            if (subSet.contains(nums[r])) {
+            if (subSets.contains(nums[r])) {
                 return true;
             }
 
-            subSet.add(nums[r]);
+            subSets.add(nums[r]);
         }
         return false;
     }
--- Array/594-Longest-Harmonious-Subsequence.java
+++ Array/594-Longest-Harmonious-Subsequence.java
@@ -2,22 +2,20 @@ import java.util.HashMap;
 
 class Solution {
     public int findLHS(int[] nums) {
-        int subLength = 0;
         HashMap<Integer, Integer> count = new HashMap<>();
+        int maxSubstring = 0;
 
         for (int i = 0; i < nums.length; i++) {
             count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
         }
 
-        System.out.println("This is count: " + count);
-
         for (int key : count.keySet()) {
             if (count.containsKey(key + 1)) {
                 int length = count.get(key) + count.get(key + 1);
-                subLength = Math.max(subLength, length);
+                maxSubstring = Math.max(maxSubstring, length);
             }
         }
 
-        return subLength;
+        return maxSubstring;
     }
 }
