25ef73d8adbb71ce6025e5ade32f5436426869dc
Jing Huang <jinghuang365@gmail.com>
2024-07-27 15:25:43 +1000

    leetcode/array: Q209 done in ts

--- Array/209-Minimum-Size-Subarray-Sum.ts
+++ Array/209-Minimum-Size-Subarray-Sum.ts
@@ -1,3 +1,19 @@
 function minSubArrayLen(target: number, nums: number[]): number {
-    return 0;
+    let l = 0;
+    let r = 0;
+    let sum = 0;
+    let minLen = Number.MAX_VALUE;
+    while (r < nums.length) {
+        if (sum < target) {
+            sum += nums[r];
+            r++;
+        }
+
+        while (sum >= target) {
+            minLen = Math.min(minLen, r - l);
+            sum -= nums[l];
+            l++;
+        }
+    }
+    return minLen === Number.MAX_VALUE ? 0 : minLen;
 };
