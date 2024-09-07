1b2ad3b1755fc34a8583c37ec5e4b2e99223c9cb
Jing Huang <jinghuang365@gmail.com>
2024-09-07 21:53:42 +1000

    leetcode/tree: move Q100 into a folder

--- Binary Tree/100-Same-Tree/Solution.java
+++ Binary Tree/100-Same-Tree/Solution.java
@@ -14,18 +14,16 @@ class Solution {
     }
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        }
-
+        if (p == null && q == null) return true;
+        // Note the case when one node is null while the other is not null.
         if (p != null && q != null) {
-            if (p.val == q.val) {
-                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-            } else {
+            if (p.val != q.val) {
                 return false;
-            }
+            }   
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         } else {
             return false;
         }
     }
+        
 }
