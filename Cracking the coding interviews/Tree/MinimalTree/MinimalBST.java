c8c1859e68ad5d1120e2980c30fb15c9f0f3d495
HenryHuang365 <jinghuang365@gmail.com>
2024-09-14 00:00:51 +1000

    cracking/tree: Minimal Tree redo

--- Cracking the coding interviews/Tree/MinimalTree/MinimalBST.java
+++ Cracking the coding interviews/Tree/MinimalTree/MinimalBST.java
@@ -1,57 +1,47 @@
 /**
  * Solution
  * 
- * Given a sorted (increasing order) array with unique integer elements, write an algorithm
+ * Given a sorted (increasing order) array with unique integer elements, write
+ * an algorithm
  * to create a binary search tree with minimal height.
  */
 // Class representing a node in the binary search tree
+
+/**
+ * InnerMinimalBST
+ */
 class TreeNode {
     int data;
     TreeNode left, right;
 
-    // Constructor to create a new node
-    TreeNode(int data) {
-        this.data = data;
-        left = right = null;
+    public TreeNode(int value) {
+        this.data = value;
+        this.left = null;
+        this.right = null;
     }
+
 }
 
 public class MinimalBST {
 
-    // Function to create a minimal height BST from a sorted array
-    TreeNode createMinimalBST(int array[]) {
-        return createMinimalBST(array, 0, array.length - 1);
+    public TreeNode createMinimalBST(int[] array) {
+        return helper(array, 0, array.length - 1);
     }
 
-    // Helper function to recursively create the BST
-    TreeNode createMinimalBST(int arr[], int start, int end) {
-        if (end < start) {
+    public TreeNode helper(int[] array, int start, int end) {
+        if (end < start)
             return null;
-        }
 
         int mid = (start + end) / 2;
-        TreeNode node = new TreeNode(arr[mid]);
+        TreeNode rootNode = new TreeNode(array[mid]);
 
-        node.left = createMinimalBST(arr, start, mid - 1);
-        node.right = createMinimalBST(arr, mid + 1, end);
+        rootNode.left = helper(array, start, mid - 1);
+        rootNode.right = helper(array, mid + 1, end);
 
-        return node;
+        return rootNode;
     }
 
-    // Function to calculate the height of the BST
-    int calculateHeight(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int leftHeight = calculateHeight(node.left);
-        int rightHeight = calculateHeight(node.right);
-
-        return Math.max(leftHeight, rightHeight) + 1;
-    }
-
-    // In-order traversal to display the tree
-    void inOrderTraversal(TreeNode node) {
+    public void inOrderTraversal(TreeNode node) {
         if (node != null) {
             inOrderTraversal(node.left);
             System.out.print(node.data + " ");
@@ -59,10 +49,20 @@ public class MinimalBST {
         }
     }
 
+    public int calculateHeight(TreeNode node) {
+        if (node == null)
+            return 0;
+
+        int leftHeight = calculateHeight(node.left);
+        int rightHeight = calculateHeight(node.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
     public static void main(String[] args) {
         MinimalBST bst = new MinimalBST();
 
-        int[] sortedArray = {1, 2, 3};
+        int[] sortedArray = { 1, 2, 3 };
 
         // Create a minimal height BST
         TreeNode root = bst.createMinimalBST(sortedArray);
