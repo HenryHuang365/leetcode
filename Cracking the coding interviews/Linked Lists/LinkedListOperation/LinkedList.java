8c584d270388d6edd9a86aceaa0a27d68ccccab1
HenryHuang365 <jinghuang365@gmail.com>
2024-09-16 00:07:44 +1000

    cracking/linked list: linked list operations

---
+++ Cracking the coding interviews/Linked Lists/LinkedListOperation/LinkedList.java
@@ -0,0 +1,81 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int value) {
+        this.data = value;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+    Node head;
+
+    // Append value to the end of the list iteratively
+    public void appendToTail(int value) {
+        if (head == null) {
+            head = new Node(value);
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = new Node(value);
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void delete(int value) {
+        if (head == null)
+            return;
+
+        if (head.data == value) {
+            head = head.next;
+        }
+
+        Node currNode = head;
+        while (currNode.next != null) {
+            if (currNode.next.data == value) {
+                // This is going to skip the first value appearance
+                currNode.next = currNode.next.next;
+                return;
+            }
+            currNode = currNode.next;
+        }
+    }
+
+    // Print the linked list iteratively
+    public void printLinkedList() {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    // Append value to the end of the list recursively
+    public void appendToTailRecursion(Node node, int value) {
+        if (node == null) {
+            head = new Node(value); // If head is null, initialize the list
+        } else if (node.next == null) {
+            node.next = new Node(value);
+        } else {
+            appendToTailRecursion(node.next, value);
+        }
+    }
+
+    // Print the linked list recursively
+    public void printLinkedListRecursion(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.data + " ");
+        printLinkedListRecursion(node.next);
+    }
+}
---
+++ Cracking the coding interviews/Linked Lists/LinkedListOperation/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.appendToTail(1);
+        list.appendToTail(2);
+        list.appendToTail(3);
+        list.prepend(0);
+        list.delete(2);
+        System.out.print("Iterative print: ");
+        list.printLinkedList(); // Iterative print
+
+        System.out.print("Recursive append and print: ");
+        list.appendToTailRecursion(list.head, 4);
+        list.printLinkedListRecursion(list.head); // Recursive print
+        System.out.println();
+    }
+}
