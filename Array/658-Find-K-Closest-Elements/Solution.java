c13632e579ab7f41622f7f3a3116880b5541ce3a
HenryHuang365 <jinghuang365@gmail.com>
2024-09-15 21:01:00 +1000

    leetcode/array: Q658 done with sliding windows

--- Array/658-Find-K-Closest-Elements/Solution.java
+++ Array/658-Find-K-Closest-Elements/Solution.java
@@ -4,16 +4,14 @@ import java.util.*;
  * Solution
  */
 class Solution {
+    // binary search
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> result = new ArrayList<>();
-
-        int left = 0, right = arr.length - k;
+        List<Integer> res = new ArrayList<>();
+        int left = 0;
+        int right = arr.length - k;
 
         while (left < right) {
             int mid = left + (right - left) / 2;
-            // System.out.println("left: " + left);
-            // System.out.println("right: " + right);
-            // System.out.println("mid: " + mid);
             if (x - arr[mid] > arr[mid + k] - x) {
                 left = mid + 1;
             } else {
@@ -22,15 +20,34 @@ class Solution {
         }
 
         for (int i = left; i < left + k; i++) {
-            result.add(arr[i]);
+            res.add(arr[i]);
         }
+        return res;
+    }
 
-        return result;
+    // sliding window
+    public List<Integer> findClosestElementsSlidingWindow(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (right - left >= k) {
+            if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = left; i < left + k; i++) {
+            res.add(arr[i]);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] input = new int[] { 1, 2, 3, 4, 5 };
-        System.out.println("Output: " + solution.findClosestElements(input, 4, 3));
+        System.out.println("Output: " + solution.findClosestElements(input, 2, 3));
+        System.out.println("Output: " + solution.findClosestElementsSlidingWindow(input, 2, 3));
     }
 }
