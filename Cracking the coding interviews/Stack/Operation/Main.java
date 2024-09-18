c74a59054b1056e0d6339fba578e406e725227d6
Jing Huang <jinghuang365@gmail.com>
2024-09-19 01:33:49 +1000

    cracking/stack: stack operation is implemented

--- Cracking the coding interviews/Stack/Operation/Main.java
+++ Cracking the coding interviews/Stack/Operation/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Stack stack = new Stack();
+        System.out.println("isEmpty: " + stack.isEmpty());
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println("isEmpty: " + stack.isEmpty());
+        System.out.print("stack: ");
+        stack.printStack();
+        System.out.println("top: " + stack.peek());
+        stack.pop();
+        System.out.print("stack: ");
+        stack.printStack();
+        System.out.println("top: " + stack.peek());
     }
 }
---
+++ Cracking the coding interviews/Stack/Operation/Stack.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Stack {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int value) {
+            this.data = value;
+            this.next = null;
+        }
+    }
+
+    private Node top;
+
+    public int pop() {
+        if (top == null) throw new EmptyStackException();
+        int value = top.data;
+        top = top.next;
+        return value;
+    }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (top != null) {
+            newNode.next = top;
+        }
+        top = newNode;
+    }
+    public int peek() {
+        if (top == null) throw new EmptyStackException();
+        return top.data;
+    }
+    
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void printStack() {
+        Node n = top;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+
+        System.out.println();
+    }
+}
