360509de1fece12b9e8f55166334728a984e56d1
HenryHuang365 <jinghuang365@gmail.com>
2024-09-21 10:45:35 +1000

    cracking/linkedlist: operation and questions

--- Cracking the coding interviews/Linked Lists/ReturnToKthToLast/LinkedList.java
+++ Cracking the coding interviews/Linked Lists/ReturnToKthToLast/LinkedList.java
@@ -1,5 +1,6 @@
-import java.util.*;
-
+/**
+ * InnerLinkedList
+ */
 class LinkedListNode {
     int data;
     LinkedListNode next;
@@ -8,7 +9,6 @@ class LinkedListNode {
         this.data = value;
         this.next = null;
     }
-
 }
 
 public class LinkedList {
@@ -19,7 +19,6 @@ public class LinkedList {
             head = new LinkedListNode(value);
             return;
         }
-
         LinkedListNode currNode = head;
         while (currNode.next != null) {
             currNode = currNode.next;
@@ -36,39 +35,6 @@ public class LinkedList {
         System.out.println();
     }
 
-    public void deleteDups(LinkedListNode n) {
-        // only have one node for iterations. Checking n.next so n is acting as a prev
-        // node. Node to add head.data to the set first.
-        HashSet<Integer> set = new HashSet<>();
-        if (n == null)
-            return;
-        set.add(n.data);
-        while (n.next != null) {
-            if (set.contains(n.next.data)) {
-                n.next = n.next.next;
-            } else {
-                set.add(n.next.data);
-                n = n.next;
-            }
-        }
-    }
-
-    public void deleteDupsTwoNodes(LinkedListNode n) {
-        // This methods has two nodes, using the runner method. Update the prev = n when
-        // n is iterating to the next node.
-        HashSet<Integer> set = new HashSet<>();
-        LinkedListNode previous = null;
-        while (n != null) {
-            if (set.contains(n.data)) {
-                previous.next = n.next;
-            } else {
-                set.add(n.data);
-                previous = n;
-            }
-            n = n.next;
-        }
-    }
-
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
 
@@ -81,7 +47,7 @@ public class LinkedList {
         linkedList.append(2);
         System.out.print("Before: ");
         linkedList.printLinkedList();
-        linkedList.deleteDups(linkedList.head);
+        // linkedList.deleteDups(linkedList.head);
         System.out.print("After: ");
         linkedList.printLinkedList();
 
@@ -96,7 +62,7 @@ public class LinkedList {
         linkedListTwo.append(2);
         System.out.print("Before: ");
         linkedListTwo.printLinkedList();
-        linkedListTwo.deleteDupsTwoNodes(linkedListTwo.head);
+        // linkedListTwo.deleteDupsTwoNodes(linkedListTwo.head);
         System.out.print("After: ");
         linkedListTwo.printLinkedList();
     }
