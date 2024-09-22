c3ae3f6fff66cc8efd09815f922f177b15dd9251
HenryHuang365 <jinghuang365@gmail.com>
2024-09-22 23:37:29 +1000

    cracking/graphl: graph implementation

---
+++ Cracking the coding interviews/Graph/Operation/Graph.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+/**
+ * Graph
+ */
+public class Graph {
+    private HashMap<Integer, Node> nodeLookup = new HashMap<>();
+
+    public class Node {
+        int id;
+        LinkedList<Node> adjacent = new LinkedList<Node>();
+
+        private Node(int id) {
+            this.id = id;
+        }
+
+    }
+
+    public Node getNode(int id) {
+        return nodeLookup.get(id);
+    }
+
+    public void addNode(int id) {
+        if (nodeLookup.containsKey(id)) {
+            return;
+        }
+        nodeLookup.put(id, new Node(id));
+    }
+
+    // add edge in a directed graph
+    public void addEdge(int source, int destination) {
+        Node s = getNode(source);
+        Node d = getNode(destination);
+        if (s == null) {
+            System.out.println("Source node " + source + " does not exist.");
+            return;
+        }
+        if (d == null) {
+            System.out.println("Destination node " + destination + " does not exist.");
+            return;
+        }
+        s.adjacent.add(d);
+        // d.adjacent.add(s); // If this is an undirected graph, add this line.
+    }
+
+    public void displayGraph() {
+        for (Integer id : nodeLookup.keySet()) {
+            Node node = getNode(id);
+            System.out.print("Node " + id + " is connected to: ");
+            for (Node adj : node.adjacent) {
+                System.out.print(adj.id + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean hasPathDFS(int source, int destination) {
+        Node s = getNode(source);
+        Node d = getNode(destination);
+        HashSet<Integer> visted = new HashSet<>();
+        return hasPathDFS(s, d, visted);
+    }
+
+    public boolean hasPathDFS(Node source, Node destination, HashSet<Integer> visted) {
+        if (visted.contains(source.id)) {
+            return false;
+        }
+        visted.add(source.id);
+        if (source.id == destination.id) {
+            return true;
+        }
+
+        for (Node child : source.adjacent) {
+            if (hasPathDFS(child, destination, visted)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ Cracking the coding interviews/Graph/Operation/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+        // Adding nodes to the graph
+        graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.addNode(5);
+
+        // Attempting to add a duplicate node
+        graph.addNode(3); // Should indicate that node 3 already exists
+
+        // Adding edges between nodes
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 5);
+        // graph.addEdge(5, 1); // Creating a cycle
+        graph.addEdge(6, 1); // Attempting to add an edge with a non-existent source node
+
+        // Displaying the graph's adjacency list
+        System.out.println("\nAdjacency List of the Graph:");
+        graph.displayGraph();
+
+        System.out.println("It is " + graph.hasPathDFS(1, 2) + " that Node 1 has a path to Node 2.");
+        System.out.println("It is " + graph.hasPathDFS(5, 3) + " that Node 5 has a path to Node 3.");
+    }
+}
