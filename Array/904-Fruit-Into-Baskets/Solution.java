6ce4a3ee048544c5effef8047a5985b0ae8e5c88
HenryHuang365 <jinghuang365@gmail.com>
2024-09-22 13:49:32 +1000

    leetcode/array&dp: Q837 and Q904

---
+++ Array/904-Fruit-Into-Baskets/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ DP/837-New-21-Game/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        return 3.0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
