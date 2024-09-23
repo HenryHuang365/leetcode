b3f56411e34f406d41341b4e02868378c47369a7
Jing Huang <jinghuang365@gmail.com>
2024-09-23 12:23:20 +1000

    cracking/graph: graph implementation add hasPathBFS

--- Cracking the coding interviews/Graph/Operation/Graph.java
+++ Cracking the coding interviews/Graph/Operation/Graph.java
@@ -78,4 +78,33 @@ public class Graph {
         return false;
     }
 
+    public boolean hasPathBFS(int source, int destination) {
+        return hasPathBFS(getNode(source), getNode(destination));
+    }
+
+    public boolean hasPathBFS(Node source, Node destination) {
+        LinkedList<Node> nextToVisit = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        nextToVisit.add(source);
+
+        while (!nextToVisit.isEmpty()) {
+            Node node = nextToVisit.remove();
+            if (node == destination) {
+                return true;
+            }
+
+            if (visited.contains(node.id)) {
+                continue;
+            }
+
+            visited.add(node.id);
+
+            for (Node child : node.adjacent) {
+                nextToVisit.add(child);
+            }
+        }
+
+        return false;
+    }
+
 }
--- Cracking the coding interviews/Graph/Operation/Main.java
+++ Cracking the coding interviews/Graph/Operation/Main.java
@@ -27,5 +27,8 @@ public class Main {
 
         System.out.println("It is " + graph.hasPathDFS(1, 2) + " that Node 1 has a path to Node 2.");
         System.out.println("It is " + graph.hasPathDFS(5, 3) + " that Node 5 has a path to Node 3.");
+
+        System.out.println("It is " + graph.hasPathBFS(1, 2) + " that Node 1 has a path to Node 2.");
+        System.out.println("It is " + graph.hasPathBFS(5, 3) + " that Node 5 has a path to Node 3.");
     }
 }
