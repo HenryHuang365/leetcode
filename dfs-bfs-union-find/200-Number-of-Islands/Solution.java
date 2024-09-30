d6776c769f7f22c7f0f7e298076c96c29a740693
Jing Huang <jinghuang365@gmail.com>
2024-09-30 22:16:33 +1000

    leetcode/dfs&bfs: Q200 redo

--- dfs-bfs-union-find/200-Number-of-Islands/Solution.java
+++ dfs-bfs-union-find/200-Number-of-Islands/Solution.java
@@ -32,16 +32,18 @@ public class Solution {
     }
 
     public void dfs(char[][] grid, int i, int j) {
-        if ((i < 0 || i >= grid.length) ||
-                (j < 0 || j >= grid[0].length) ||
-                grid[i][j] == '0') {
+        if (
+            (i < 0 || i >= grid.length) ||
+            (j < 0 || j >= grid[0].length) ||
+            grid[i][j] == '0'
+        ) {
             return;
         }
         grid[i][j] = '0';
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i, j - 1); // left
-        dfs(grid, i, j + 1); // right
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 
     public static void main(String[] args) {
