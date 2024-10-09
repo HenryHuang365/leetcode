41cfacb3318b008f022227e0a54a6290ac7bdfcc
HenryHuang365 <jinghuang365@gmail.com>
2024-10-09 23:51:19 +1100

    leetcode/dfs&bfs: Q733 done using bfs

--- dfs-bfs-union-find/733-Flood-Fill/Solution.java
+++ dfs-bfs-union-find/733-Flood-Fill/Solution.java
@@ -4,41 +4,44 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int originalColor = image[sr][sc];
+        if (originalColor == color) {
+            return image;
+        }
         Queue<int[]> nextToVisit = new LinkedList<>();
-        HashSet<int[]> visited = new HashSet<>();
+        HashSet<String> visited = new HashSet<>();
 
-        nextToVisit.offer(new int[] {sr, sc});
-        int[][] directions = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        nextToVisit.offer(new int[] { sr, sc });
+        int[][] directions = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
         while (!nextToVisit.isEmpty()) {
             int[] cell = nextToVisit.poll();
             int newSr = cell[0];
             int newSc = cell[1];
-            if (visited.contains(new int[] {newSr, newSc})) {
+            String cellString = newSr + "," + newSc;
+            if (visited.contains(cellString)) {
                 continue;
             }
-            visited.add(new int[] {newSr, newSc});
-            image[newSr][newSc] = newColor;
+            visited.add(cellString);
+            image[newSr][newSc] = color;
             for (int[] dir : directions) {
                 int adjSr = newSr + dir[0];
                 int adjSc = newSc + dir[1];
-                if (adjSr >= 0 && adjSr < image.length && adjSc >= 0 && adjSc < image.length) {
-                    if (image[adjSr][adjSc] == originalColor && !visited.contains(new int[] {adjSr, adjSc})) {
-                        nextToVisit.offer(new int[] {adjSr, adjSc});
+                if (adjSr >= 0 && adjSr < image.length && adjSc >= 0 && adjSc < image[0].length) {
+                    if (image[adjSr][adjSc] == originalColor && !visited.contains(adjSr + "," + adjSc)) {
+                        nextToVisit.offer(new int[] { adjSr, adjSc });
                     }
                 }
             }
-            
         }
         return image;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        // int[][] image = new int[][] {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
-        int[][] image = new int[][] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-        int[][] newImage = solution.floodFill(image, 0, 0, 0);
+        int[][] image = new int[][] { { 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 } };
+        // int[][] image = new int[][] { { 0, 0, 0 }, { 0, 0, 0 } };
+        int[][] newImage = solution.floodFill(image, 1, 1, 2);
         for (int[] row : newImage) {
             System.out.println("Output: " + Arrays.toString(row));
         }
