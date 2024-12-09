655bf2355e90d6d506e5b3d4c27b237b3ad2f4bf
Jing Huang <jinghuang365@gmail.com>
2024-12-09 22:49:06 +1100

    leetcode/array: Q90

--- Array/90-Subsets-II/90-Subsets-II.java
+++ Array/90-Subsets-II/90-Subsets-II.java
@@ -31,13 +31,26 @@ class Solution {
     }
 
     public void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        // if (list.contains(new ArrayList<>(tempList))) return; // This way works, but not good for performance
+        // checking the duplicates take O(n), and it is filtering not preventing. This means I am doing extra work, but I am just simply filtering out the expected results.
         list.add(new ArrayList<>(tempList));
+        
         for (int i = start; i < nums.length; i++) {
+            // This is a better way to check duplicates as it is preventing from doing redudant work and comparison is simple
+            // skips the duplicates
             if (i > start && nums[i] == nums[i - 1])
                 continue;
             tempList.add(nums[i]);
             backtrack(list, tempList, nums, i + 1);
             tempList.remove(tempList.size() - 1);
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = new int[] {1, 2, 1};
+
+        System.out.println("output: " + solution.subsetsWithDup(nums));
     }
 }
