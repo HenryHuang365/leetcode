cce124ce7e626733b320d2556a2aeaa55e42c0e4
HenryHuang365 <jinghuang365@gmail.com>
2024-12-15 21:34:12 +1100

    leetcode/bfs&dfs: Q733 redo the questio

--- dfs-bfs-union-find/733-Flood-Fill/Solution.java
+++ dfs-bfs-union-find/733-Flood-Fill/Solution.java
@@ -4,33 +4,31 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int originalColor = image[sr][sc];
-        if (originalColor == color) {
-            return image;
-        }
+    public int[][] floodFillBFS(int[][] image, int sr, int sc, int color) {
         Queue<int[]> nextToVisit = new LinkedList<>();
-        HashSet<String> visited = new HashSet<>();
+        nextToVisit.add(new int[] { sr, sc });
+        HashSet<String> visted = new HashSet<String>();
 
-        nextToVisit.offer(new int[] { sr, sc });
-        int[][] directions = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+        int originalColor = image[sr][sc];
+        int[][] directions = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
         while (!nextToVisit.isEmpty()) {
             int[] cell = nextToVisit.poll();
-            int newSr = cell[0];
-            int newSc = cell[1];
-            String cellString = newSr + "," + newSc;
-            if (visited.contains(cellString)) {
+            int row = cell[0];
+            int col = cell[1];
+            if (visted.contains(row + ", " + col)) {
                 continue;
             }
-            visited.add(cellString);
-            image[newSr][newSc] = color;
+            visted.add(row + ", " + col);
+            image[row][col] = color;
+
             for (int[] dir : directions) {
-                int adjSr = newSr + dir[0];
-                int adjSc = newSc + dir[1];
-                if (adjSr >= 0 && adjSr < image.length && adjSc >= 0 && adjSc < image[0].length) {
-                    if (image[adjSr][adjSc] == originalColor && !visited.contains(adjSr + "," + adjSc)) {
-                        nextToVisit.offer(new int[] { adjSr, adjSc });
-                    }
+                int r = row + dir[0];
+                int c = col + dir[1];
+
+                if (r >= 0 && r < image.length && c >= 0 && c < image[0].length && image[r][c] == originalColor
+                        && !visted.contains(r + ", " + c)) {
+                    nextToVisit.add(new int[] { r, c });
                 }
             }
         }
@@ -61,7 +59,7 @@ public class Solution {
         Solution solution = new Solution();
         int[][] image = new int[][] { { 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 } };
         // int[][] image = new int[][] { { 0, 0, 0 }, { 0, 0, 0 } };
-        int[][] newImageBFS = solution.floodFill(image, 1, 1, 2);
+        int[][] newImageBFS = solution.floodFillBFS(image, 1, 1, 2);
         int[][] newImageDFS = solution.floodFillDFS(image, 1, 1, 2);
         System.out.println("BFS: ");
         for (int[] row : newImageBFS) {
