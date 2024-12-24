c7a09fa390dd2c5fdba1dccbe27aaaf3ffc9807b
HenryHuang365 <jinghuang365@gmail.com>
2024-12-24 15:07:32 +1100

    leetcode/bfs&dfs: Q1162 redo using bfs

--- dfs-bfs-union-find/1162-As-Far-from-Land-as-Possible/Solution.java
+++ dfs-bfs-union-find/1162-As-Far-from-Land-as-Possible/Solution.java
@@ -5,45 +5,44 @@ import java.util.*;
  */
 public class Solution {
     public int maxDistance(int[][] grid) {
-        int N = grid.length;
-        Queue<int[]> nextToVisited = new LinkedList<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        int n = grid.length;
+        Queue<int[]> nextToVisit = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    nextToVisited.offer(new int[]{i, j});
+                    nextToVisit.offer(new int[] { i, j });
                 }
             }
         }
 
-        if (nextToVisited.size() == N * N || nextToVisited.size() == 0) {
+        if (nextToVisit.isEmpty() || nextToVisit.size() == n * n) {
             return -1;
         }
 
-        int[][] directions = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int res = -1; // The initial value of res will not affect the result
+        int result = -1;
+        int[][] directions = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        while (!nextToVisit.isEmpty()) {
+            int[] cell = nextToVisit.poll();
+            int row = cell[0];
+            int col = cell[1];
+            result = grid[row][col];
 
-        while (!nextToVisited.isEmpty()) {
-            int[] cell = nextToVisited.poll(); 
-            int x = cell[0];
-            int y = cell[1];
-            res = grid[x][y];
             for (int[] dir : directions) {
-                int newX = x + dir[0];
-                int newY = y + dir[1];
-                if (newX >= 0 && newX < N && newY >= 0 && newY < N && grid[newX][newY] == 0) {
-                    grid[newX][newY] = grid[x][y] + 1;
-                    nextToVisited.offer(new int[]{newX, newY});
+                int r = row + dir[0];
+                int c = col + dir[1];
+                if (r >= 0 && r < n && c >= 0 && c < n && grid[r][c] == 0) {
+                    nextToVisit.offer(new int[] { r, c });
+                    grid[r][c] = grid[row][col] + 1;
                 }
             }
         }
-        return res - 1;
+        return result - 1;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] grid = {{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
-        int[][] grid2 = {{1, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        int[][] grid = { { 1, 0, 1 }, { 0, 0, 0 }, { 1, 0, 1 } };
+        int[][] grid2 = { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
         System.out.println("Output: " + solution.maxDistance(grid));
         System.out.println("Output: " + solution.maxDistance(grid2));
     }
