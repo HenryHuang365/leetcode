e635cab25115a0f901bf07a739a86d5003dd3b00
HenryHuang365 <jinghuang365@gmail.com>
2025-02-15 14:49:05 +1100

    leetcode/backtracking: Q79 optimised

--- Backtracking/79-word-search/Solution.java
+++ Backtracking/79-word-search/Solution.java
@@ -46,9 +46,45 @@ class Solution {
         }
     }
 
+    public boolean existTwo(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfsTwo(board, i, j, 0, word))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfsTwo(char[][] board, int i, int j, int index, String word) {
+        if (index == word.length())
+            return true;
+
+        if ((i < 0 || i >= board.length) ||
+                (j < 0 || j >= board[0].length) ||
+                (board[i][j] == '0') ||
+                (board[i][j] != word.charAt(index))) {
+            return false;
+        }
+        Character letter = board[i][j];
+        board[i][j] = '0';
+        boolean res = dfsTwo(board, i + 1, j, index + 1, word) ||
+                dfsTwo(board, i - 1, j, index + 1, word) ||
+                dfsTwo(board, i, j + 1, index + 1, word) ||
+                dfsTwo(board, i, j - 1, index + 1, word);
+
+        board[i][j] = letter;
+        return res;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         char[][] board = new char[][] { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
-        System.out.println("Output: " + solution.exist(board, "SEE"));
+        System.out.println("Output: " + solution.exist(board, "ABCCED"));
+
+        char[][] boardTwo = new char[][] { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
+        System.out.println("Output: " + solution.existTwo(boardTwo, "ABCCED"));
     }
 }
