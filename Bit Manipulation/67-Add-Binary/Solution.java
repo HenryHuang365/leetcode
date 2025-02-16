4c2f746907ac02ea69973643202134d2469bf693
HenryHuang365 <jinghuang365@gmail.com>
2025-02-16 15:59:25 +1100

    leetcode/bit: rename files

--- Bit Manipulation/67-Add-Binary/Solution.java
+++ Bit Manipulation/67-Add-Binary/Solution.java
@@ -3,11 +3,17 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder s = new StringBuilder();
-        int i = a.length() - 1; int j = b.length() - 1; int carry = 0;        
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
         while (i >= 0 || j >= 0) {
             int sum = carry;
-            if (i >= 0) {sum += a.charAt(i--) - '0';}
-            if (j >= 0) {sum += b.charAt(i--) - '0';}
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(i--) - '0';
+            }
             s.append(sum == 2 || sum == 0 ? 0 : 1);
             carry = sum == 2 || sum == 0 ? 0 : 1;
         }
