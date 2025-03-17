54ccdf5785a106ec1d9adc6e13407fe5f630fd8f
Jing Huang <jinghuang365@gmail.com>
2025-03-17 23:53:01 +1100

    leetcode/linkedList: Q160 rename file

--- Linked List/160-Intersection-of-Two-Linked-Lists/ListNode.java
+++ Linked List/160-Intersection-of-Two-Linked-Lists/ListNode.java
@@ -2,7 +2,6 @@
 
 // For example, the following two linked lists begin to intersect at node c1:
 
-
 // The test cases are generated such that there are no cycles anywhere in the entire linked structure.
 
 // Note that the linked lists must retain their original structure after the function returns.
@@ -18,10 +17,7 @@
 // skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
 // The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
 
- 
-
 // Example 1:
-
 
 // Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
 // Output: Intersected at '8'
@@ -30,19 +26,16 @@
 // - Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
 // Example 2:
 
-
 // Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
 // Output: Intersected at '2'
 // Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
 // From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
 // Example 3:
 
-
 // Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
 // Output: No intersection
 // Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
 // Explanation: The two lists do not intersect, so return null.
- 
 
 // Constraints:
 
@@ -55,34 +48,31 @@
 // intersectVal is 0 if listA and listB do not intersect.
 // intersectVal == listA[skipA] == listB[skipB] if listA and listB intersect.
 
-
-
 // Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
 
 class Solution {
-
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
-    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         // Solution 1: Brute Force Solution
         // while (headA != null) {
-        //     ListNode temp = headB;
-        //     while (temp != null) {
-        //         if (headA == temp) {
-        //             return headA;
-        //         }
+        // ListNode temp = headB;
+        // while (temp != null) {
+        // if (headA == temp) {
+        // return headA;
+        // }
 
-        //         temp = temp.next;
-        //     }
+        // temp = temp.next;
+        // }
 
-        //     headA = headA.next;
+        // headA = headA.next;
         // }
         // return null;
 
@@ -92,34 +82,34 @@ class Solution {
         // ListNode ptr1 = headA;
         // ListNode ptr2 = headB;
         // while (ptr1 != null) {
-        //     ptr1 = ptr1.next;
-        //     n++;
+        // ptr1 = ptr1.next;
+        // n++;
         // }
         // while (ptr2 != null) {
-        //     ptr2 = ptr2.next;
-        //     m++;
+        // ptr2 = ptr2.next;
+        // m++;
         // }
 
         // int dif = Math.abs(n-m);
         // if (n > m) {
-        //     while (dif > 0) {
-        //         headA = headA.next;
-        //         dif--;
-        //     }
+        // while (dif > 0) {
+        // headA = headA.next;
+        // dif--;
+        // }
         // } else {
-        //     while (dif > 0) {
-        //         headB = headB.next;
-        //         dif--;
-        //     }
+        // while (dif > 0) {
+        // headB = headB.next;
+        // dif--;
+        // }
         // }
 
         // while (headA != null && headB != null) {
-        //     if (headA == headB) {
-        //         return headA;
-        //     }
+        // if (headA == headB) {
+        // return headA;
+        // }
 
-        //     headA = headA.next;
-        //     headB = headB.next;
+        // headA = headA.next;
+        // headB = headB.next;
         // }
         // return null;
         if (headA == null || headB == null) {
