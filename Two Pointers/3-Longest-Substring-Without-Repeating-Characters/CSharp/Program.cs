ca7d79048d794e85a65166e9e925c38aef6fd8cf
Jing Huang <jinghuang365@gmail.com>
2025-03-21 21:23:52 +1100

    leetcode/two-pointers: q3 done in c#

---
+++ Two Pointers/3-Longest-Substring-Without-Repeating-Characters/CSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        Console.WriteLine(program.LengthOfLongestSubstring("bbbbb"));
+    }
+
+    public int LengthOfLongestSubstring(string s)
+    {
+        int res = 0;
+        int left = 0;
+        int right = 0;
+        HashSet<char> set = new HashSet<char>();
+
+        while (right < s.Length)
+        {
+            char c = s[right];
+            if (!set.Contains(c))
+            {
+                set.Add(c);
+                right++;
+            }
+            else
+            {
+                while (set.Contains(c) && left <= right)
+                {
+                    set.Remove(s[left]);
+                    left++;
+                }
+            }
+
+            res = Math.Max(res, right - left);
+        }
+
+        return res;
+    }
+}
