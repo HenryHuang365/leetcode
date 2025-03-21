7350699f8e938c99a9e38abbda688e40928a567a
Jing Huang <jinghuang365@gmail.com>
2025-03-21 20:45:48 +1100

    leetcode/two-pointers: Q11 done in c#

---
+++ Two Pointers/11-Container-With-Most-Water/CSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] height = { 1, 1 };
+
+        Program program = new Program();
+        Console.WriteLine(program.MaxArea(height));
+    }
+
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.Length - 1;
+
+        while (right > left)
+        {
+            int h = Math.Min(height[right], height[left]);
+            int w = right - left;
+
+            maxArea = Math.Max(maxArea, h * w);
+
+            if (height[left] <= height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
