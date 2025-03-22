fb8cb3f4688fd377f18db87d4f178b746fda52a1
Jing Huang <jinghuang365@gmail.com>
2025-03-22 17:30:21 +1100

    leetcode/backtracking: Q47 permutation 2 done in c#

---
+++ Backtracking/46-Permutations/CSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        int[] nums = { 1, 2, 3 };
+        IList<IList<int>> list = program.Permute(nums);
+        foreach (var path in list)
+        {
+            foreach (var num in path)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        IList<IList<int>> list = new List<IList<int>>();
+        List<int> path = new List<int>();
+
+        Backtracking(nums, list, path);
+        return list;
+    }
+
+    public void Backtracking(int[] nums, IList<IList<int>> list, List<int> path)
+    {
+        if (path.Count == nums.Length)
+        {
+            list.Add(new List<int>(path));
+        }
+        else
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (path.Contains(nums[i])) continue;
+
+                path.Add(nums[i]);
+                Backtracking(nums, list, path);
+                path.RemoveAt(path.Count - 1);
+            }
+        }
+    }
+}
