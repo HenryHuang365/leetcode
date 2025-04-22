f250455f1ccae9504b2ba018ea0c5bb89332c671
HenryHuang365 <jinghuang365@gmail.com>
2025-04-22 12:13:53 +1000

    leetcode/linkedlist: Q206 moved the old solution into java and create a new c# solution

---
+++ Linked List/206-Reverse-Linked-List/CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode? next;
+    public ListNode(int val, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Hello World");
+    }
+}
