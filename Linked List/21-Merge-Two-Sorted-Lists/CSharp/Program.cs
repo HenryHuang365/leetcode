90280c26518666142eaafd0ac35de19102a8854e
Jing Huang <jinghuang365@gmail.com>
2025-06-09 16:41:03 +1000

    leetcode/linkedlist: Q21 done

--- Linked List/21-Merge-Two-Sorted-Lists/CSharp/Program.cs
+++ Linked List/21-Merge-Two-Sorted-Lists/CSharp/Program.cs
@@ -49,25 +49,61 @@ public class LinkedList
 
 class Program
 {
-    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2)
     {
-        return list1;
+        if (list1 == null)
+        {
+            return list2;
+        }
+        else if (list2 == null)
+        {
+            return list1;
+        }
+
+        ListNode res = new ListNode(0);
+        ListNode currNode = res;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val >= list2.val)
+            {
+                currNode.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            else
+            {
+                currNode.next = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+
+            currNode = currNode.next;
+        }
+
+        currNode.next = list1 == null ? list2 : list1;
+
+        return res.next;
     }
     static void Main()
     {
-        LinkedList list = new LinkedList();
-        list.appendToTail(0);
-        list.appendToTail(1);
-        list.appendToTail(2);
-        list.appendToTail(3);
-        Console.Write("Before: ");
-        list.printLinkedList();
+        LinkedList list1 = new LinkedList();
+        list1.appendToTail(1);
+        list1.appendToTail(2);
+        list1.appendToTail(4);
 
+        LinkedList list2 = new LinkedList();
+        list2.appendToTail(1);
+        list2.appendToTail(3);
+        list2.appendToTail(4);
+
+        Console.WriteLine("Before: ");
+        list1.printLinkedList();
+        Console.WriteLine("");
+        list2.printLinkedList();
         Console.WriteLine("");
 
         Program program = new Program();
-        ListNode? newHead = program.MergeTwoLists(list.head ?? new ListNode(0), list.head ?? new ListNode(0));
-        Console.Write("After: ");
+        ListNode? newHead = program.MergeTwoLists(list1.head, list2.head);
+        Console.Write("Merged: ");
         while (newHead != null)
         {
             Console.Write(newHead.val + " ");
